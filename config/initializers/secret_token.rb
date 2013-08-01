d6ab26b292cd31858c4f32d6d0497013c3a7e983
Spencer <spencerartisan@gmail.com>
2013-08-01 22:45:28 +0100

    Play rails 4 app to store links

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4::Application.config.secret_key_base = '79a106a5a8ebe51415e56535229c34268192230e2759617125b280054ff6d6e3db0bdfcdb637468be29225b73dc96e4ffa316c9bec2e45c7d25b5d6522f344c7'
